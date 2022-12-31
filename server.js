2cdb2c8645d0fcc6aab1e7cb593600e72a6a307c
Shashank Mishra <shashank.mishra@bjtmail.com>
2022-12-31 18:07:22 +0530

    change: serve statics

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "build", "public", "index.html")
-    );
+    res.json({
+      msg: "server started",
+    });
   });
 }
 
