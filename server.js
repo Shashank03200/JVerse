e6bdb298ab5a6a5fcd1e887b2cab059e7c0acd81
Shashank03200 <55805726+Shashank03200@users.noreply.github.com>
2023-02-11 23:03:01 +0530

    Add: cors config

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ require("./helpers/init_mongodb");
 require("./helpers/init_redis");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}));
 app.use(express.json());
 
 app.use(morgan("dev"));
